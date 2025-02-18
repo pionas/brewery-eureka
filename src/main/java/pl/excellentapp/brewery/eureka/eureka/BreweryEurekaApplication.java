9df7d16f6411187974f613459cbbcc257a96e1e9
Adrian Pionka <adrian@pionka.com>
2025-02-18 21:13:44 +0100

    init brewery eureka server

---
+++ src/main/java/pl/excellentapp/brewery/eureka/eureka/BreweryEurekaApplication.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.eureka.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/excellentapp/brewery/eureka/eureka/BreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.eureka.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
