287ce07ae0264431f5e9d7c87705c4c9b47a4df3
Adrian Pionka <adrian@pionka.com>
2025-02-18 21:19:11 +0100

    add spring security starter

---
+++ src/main/java/pl/excellentapp/brewery/eureka/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package pl.excellentapp.brewery.eureka.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(withDefaults())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(withDefaults()).build();
+    }
+}
