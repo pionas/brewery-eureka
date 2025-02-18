4469339bd7acb3e3eb1a15873c89a9e22d92cfcc
Adrian Pionka <adrian@pionka.com>
2025-02-18 21:25:56 +0100

    Enable Eureka Server

--- src/main/java/pl/excellentapp/brewery/eureka/BreweryEurekaApplication.java
+++ src/main/java/pl/excellentapp/brewery/eureka/BreweryEurekaApplication.java
@@ -2,7 +2,9 @@ package pl.excellentapp.brewery.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class BreweryEurekaApplication {
 
