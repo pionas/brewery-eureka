84fc4cc155f30cb7098ab59668ab82c477bc18dc
Adrian Pionka <adrian@pionka.com>
2025-02-19 08:52:49 +0100

    disable csrf

--- src/main/java/pl/excellentapp/brewery/eureka/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/pl/excellentapp/brewery/eureka/infrastructure/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,8 +16,9 @@ class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .httpBasic(withDefaults()).build();
+                .httpBasic(withDefaults())
+                .build();
     }
 }
